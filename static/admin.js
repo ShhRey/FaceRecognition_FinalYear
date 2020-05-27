7e5dc7686c3e464fe490549f0ce9a74a96dc853b
Shrey Trivedi <41701281+ShreyT1257@users.noreply.github.com>
2020-05-27 20:30:36 +0530

    Add files via upload
    CSS and JavaScript Files

---
+++ static/admin.js
@@ -0,0 +1,14 @@
+function show(){
+    document.getElementById("formm").style.visibility = "visible";
+    document.getElementById("formm1").style.visibility = "hidden";
+}
+
+function hide(){
+    document.getElementById("formm").style.visibility = "hidden";
+    document.getElementById("formm1").style.visibility = "hidden";
+}
+
+function show1(){
+    document.getElementById("formm").style.visibility = "hidden";
+    document.getElementById("formm1").style.visibility = "visible";
+}
